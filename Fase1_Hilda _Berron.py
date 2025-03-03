2696f906e7d274a4456d2d5371d99f257f0cc8fe
Melatica <al02000854@tecmilenio.mx>
2025-03-02 22:32:46 -0600

    Update Fase1_Hilda _Berron.py

--- Fase1_Hilda _Berron.py
+++ Fase1_Hilda _Berron.py
@@ -89,7 +89,7 @@ def ver_lista_clientes():
 def menu():
     cargar_clientes()
     while True:
-        print("\nMenú Clientes Amazon:")
+        print("\nMenú Clientes SKY:")
         print("1. Ver lista de clientes")
         print("2. Ver cliente")
         print("3. Crear cliente nuevo")
